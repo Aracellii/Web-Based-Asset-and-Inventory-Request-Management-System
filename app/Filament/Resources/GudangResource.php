8f7115ad4f472e56535ab4476b08214edcc5bbbd
Luthfi Alaudin Rosyid <123230082@student.upnyk.ac.id>
2026-01-26 10:30:57 +0700

    Revert Soft Delete

--- app/Filament/Resources/GudangResource.php
+++ app/Filament/Resources/GudangResource.php
@@ -215,6 +215,7 @@ public static function form(Form $form): Form
             return $count > 0 ? (string)$count : null; }
             
 
+            
     public static function getPages(): array
     {
         return [
