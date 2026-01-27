e2a0086333886bb747381f52b2ad1ed74ec3f995
Luthfi Alaudin Rosyid <123230082@student.upnyk.ac.id>
2026-01-27 11:39:12 +0700

    Fix Time

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Carbon;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +20,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        
+        // Set Carbon locale to Indonesian and timezone to WIB
+        Carbon::setLocale('id');
+        date_default_timezone_set('Asia/Jakarta');
     }
 }
