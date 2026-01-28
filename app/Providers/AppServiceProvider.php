979c7333aef268bce37177a2079e984710d203cf
Luthfi Alaudin Rosyid <123230082@student.upnyk.ac.id>
2026-01-28 09:39:16 +0700

    coba ngrok

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 use Illuminate\Support\Carbon;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
+use Illuminate\Support\Facades\Request;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,5 +25,9 @@ class AppServiceProvider extends ServiceProvider
         // Set Carbon locale to Indonesian and timezone to WIB
         Carbon::setLocale('id');
         date_default_timezone_set('Asia/Jakarta');
+
+        if (Request::header('x-forwarded-proto') === 'https' || str_contains(Request::header('host'), 'ngrok-free.app')) {
+            URL::forceScheme('https');
+        }
     }
 }
