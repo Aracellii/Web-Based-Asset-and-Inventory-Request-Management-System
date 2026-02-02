db7d1d5d7120d12bd251996955d801e79563fbe3
Luthfi Alaudin Rosyid <123230082@student.upnyk.ac.id>
2026-02-02 11:48:43 +0700

    Update logo.blade.php

--- resources/views/filament/components/logo.blade.php
+++ resources/views/filament/components/logo.blade.php
@@ -1,9 +1,9 @@
 @if((request()->routeIs('filament.admin.auth.login')) || (request()->routeIs('filament.admin.auth.register')))
-<div class="flex flex-col items-center gap-4 text-center pb-3">
+<div class="flex flex-col items-center gap-4 text-center pb-4">
     <img
         src="{{ asset('build/assets/logo.svg') }}"
         alt="SIATK"
-        class="w-20 h-20 object-contain">
+        class="w-16 h-16 object-contain">
     <div class="leading-tight">
         <div class="text-2xl font-bold text-gray-900 dark:text-white uppercase tracking-tight">
             Sistem Informasi Aset & Tata Kelola
